1e2a0da69a173e8d4d71b098478c852344a8ba7b
graciaskas <makaveligracias@aol.com>
2022-06-15 15:14:46 +0200

    updated

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -4,51 +4,49 @@ import React from "react";
 const Serives = () => {
   return (
     <>
-      <div id="service">
+      <section className="service__section">
         <div className="container">
-          <div className="row">
-            <div className="col-md-6 col-12">
-              <h2 className="service-heading">OUR</h2>
-              <h2 className="services-heading2">SERVICES</h2>
-            </div>
-            <div className="col-md-6 col-12">
-              <img src="/img/rec53.png" className="serviceimg" alt="imag" />
-            </div>
+          <div className="service__content">
+            <div className="row">
+              <div className="col-md-6 col-12">
+                <h2 className="service-heading">OUR</h2>
+                <h2 className="services-heading2">SERVICES</h2>
+              </div>
+              </div>
+              <div className="col-md-6 col-12">
+                <img src="/img/rec53.png" className="serviceimg" alt="imag" />
+              </div>
           </div>
-        </div>
+       
 
-        <div className="container">
-          <div className="row">
-            <div className="col-md-4 col-12">
-              <h3 className="servehead">REAL ESTATE CONSULTANTS</h3>
-              <div className="line2"></div>
-              <p className="servepara">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Molestias et dolor explicabo? Aut recusandae quisquam
-                perspiciatis, dolor aperiam laboriosam magni.
-              </p>
-            </div>
-            <div className="col-md-4 col-12">
-              <h3 className="servehead">REAL ESTATE CONSULTANTS</h3>
-              <div className="line2"></div>
-              <p className="servepara">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Molestias et dolor explicabo? Aut recusandae quisquam
-                perspiciatis, dolor aperiam laboriosam magni.
-              </p>
-            </div>
-            <div className="col-md-4 col-12">
-              <h3 className="servehead">REAL ESTATE CONSULTANTS</h3>
-              <div className="line2"></div>
-              <p className="servepara">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Molestias et dolor explicabo? Aut recusandae quisquam
-                perspiciatis, dolor aperiam laboriosam magni.
-              </p>
-            </div>
+            <div className="service__content">
+              <div className="__service">
+                <h4 >REAL ESTATE CONSULTANTS</h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Molestias et dolor explicabo? Aut recusandae quisquam
+                  perspiciatis, dolor aperiam laboriosam magni.
+                </p>
+              </div>
+              <div className="__service">
+                <h4>REAL ESTATE CONSULTANTS</h4>
+                <p>
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Molestias et dolor explicabo? Aut recusandae quisquam
+                  perspiciatis, dolor aperiam laboriosam magni.
+                </p>
+              </div>
+              <div className="__service">
+                <h4 >REAL ESTATE CONSULTANTS</h4>
+                <p className="servepara">
+                  Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                  Molestias et dolor explicabo? Aut recusandae quisquam
+                  perspiciatis, dolor aperiam laboriosam magni.
+                </p>
+              </div>
           </div>
-        </div>
-      </div>
+         </div>
+      </section>
     </>
   );
 };
